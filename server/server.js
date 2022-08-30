a60e9e23fa3cc0cc869ccdd4e6eab20b6be583d9
jimmykt <jimmykkst@gmail.com>
2022-08-30 18:34:17 -0400

    backend db

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 5005;
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "rootcap8",
+  database: "AlbumsDB",
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome to albums");
+});
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
